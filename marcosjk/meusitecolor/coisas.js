a79f3c5f2672e3411f2ef7b1bd0813576942d688
marc0sjk <marcosjka9@gmail.com>
2023-10-19 21:56:18 -0300

    arrumado

--- marcosjk/meusitecolor/coisas.js
+++ marcosjk/meusitecolor/coisas.js
@@ -1,8 +1,29 @@
 
 let button = document.getElementById("darklight")
-let body = document.querySelector("body")
-let th = document.querySelectorAll("th")
+let fundopag = document.querySelector("body")
+let dtbcat = document.querySelectorAll("th.tabcateg")
+let dinftab = document.querySelectorAll("th.tabinfo")
+let titum = document.querySelectorAll("h1.titulos")
+let titu = document.querySelectorAll("h2.titulos")
+let parag = document.querySelectorAll("p")
+let imgbord = document.getElementById("roundimg")
 button.addEventListener("click",()=> {
-     body.classList.toggle("dark")
-     body.classList.toggle("dtab")
+     fundopag.classList.toggle("dark")
+     imgbord.classList.toggle("dimgborder")
+
+     dtbcat.forEach((coisa) =>{
+          coisa.classList.toggle("dtab")
+     })
+     dinftab.forEach((element) => {
+          element.classList.toggle("dtab")
+     })
+     titum.forEach((h1) => {
+          h1.classList.toggle("dtitle")
+     })
+     titu.forEach((h2) => {
+          h2.classList.toggle("dtitle")
+     })
+     parag.forEach((p) => {
+          p.classList.toggle("dtab")
+     })
 })
