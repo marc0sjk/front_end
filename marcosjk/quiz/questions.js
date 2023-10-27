739d7bd2db4144d116c44b844820b000a2b3b3e3
Marc0sJK <marcosjka9@gmail.com>
2023-10-27 06:40:51 -0300

    parte revisada

--- marcosjk/quiz/questions.js
+++ marcosjk/quiz/questions.js
@@ -17,33 +17,25 @@ question:"qual tag cria uma linha orizontal?",
 options:["strong","i","hr","span"],
 answear: 2,
 }
-]
-let q4 =
-[
+,
 { 
 question:"quais são os elementos internos de uma tabela?",
 options:["tr e ul","tr e th","li e th","ol e tr"],
 answear: 1,
 }
-]
-let q5 =
-[
+,
 { 
 question:"qual dessas opções é um input?",
 options:["color","onclick","form","position"],
 answear: 0,
 }
-]
-let q6 =
-[
+,
 { 
 question:"qual é o ultimo caracter que define a transparência de uma propriedade rgb?",
 options:["r","t","a","s"],
 answear: 2,
 }
-]
-let q7 =
-[
+,
 { 
 question:"qual é o numero maximo de caracteres de uma propriedade exadecimal?",
 options:["3","4","5","6"],
