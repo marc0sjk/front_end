8f4c907e707f17fa8444beab4f7a1a67d80326ec
marc0sjk <marcosjka9@gmail.com>
2023-10-30 21:53:13 -0300

    concertar o erro

---
+++ projeto-exemplo/src/componentes/card/index.jsx
@@ -0,0 +1,12 @@
+const Card = ({title, content}) => {
+    return(
+        <article className="card">
+            <img src="src/assets/react.svg" alt="imagem do card"/>
+        <div>
+            <h2>{title}</h2>
+            <p>{content}</p>
+        </div>
+        <a href="www.github.com">Acessar post</a>
+        </article>
+    );
+};
---
+++ projeto-exemplo/src/pages/inicial/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";  
+function Inicial () {
+    return (
+        <div>
+            <h1>página inical</h1>
+            <Card />
+        </div>
+    );
+}
+export default Inicial;
